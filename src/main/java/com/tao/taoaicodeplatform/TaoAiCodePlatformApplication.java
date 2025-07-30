2ee356e8d0230de80ca6897d6f1dff786ff15257
T-X-1013 <cn_taoxw@foxmail.com>
2025-07-30 18:10:38 +0800

    第一次提交（后端初始化）： 1.初始化项目 2.添加依赖和配置（HuTool和 Knife4j 接口文档） 3.提供基础代码（自定义异常、请求响应包装类） 4.提供基础接口（健康检查接口）

---
+++ src/main/java/com/tao/taoaicodeplatform/TaoAiCodePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tao.taoaicodeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaoAiCodePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaoAiCodePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tao/taoaicodeplatform/TaoAiCodePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tao.taoaicodeplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaoAiCodePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
