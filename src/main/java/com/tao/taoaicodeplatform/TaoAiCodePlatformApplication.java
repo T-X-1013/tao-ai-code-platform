0d245e43b7aaec26e70530053681dfa5d60e3a55
T-X-1013 <cn_taoxw@foxmail.com>
2025-08-01 13:20:37 +0800

    第三次提交 - 前端初始化完成
    1. 开发 MyBatis Flex 代码生成器
    2. 开发核心功能，比如用户注册、用户登录、获取登录用户信息、用户注销等
    3. 全局权限校验 AOP
    4. 开发管理员增删改查接口
    5. 自定义配置解决丢失精度问题

--- src/main/java/com/tao/taoaicodeplatform/TaoAiCodePlatformApplication.java
+++ src/main/java/com/tao/taoaicodeplatform/TaoAiCodePlatformApplication.java
@@ -1,9 +1,11 @@
 package com.tao.taoaicodeplatform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tao.taoaicodeplatform.mapper")
 public class TaoAiCodePlatformApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tao/taoaicodeplatform/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.tao.taoaicodeplatform.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.tao.taoaicodeplatform.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/T-X-1013">奥德奈瑞</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
